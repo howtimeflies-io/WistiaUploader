ad678293987a4a25e60e0bcc493fa1ae43bf0afe
Huang Jian <huang.jian@howtimeflies.io>
2018-07-01 14:02:48 +0800

    Add bootstrap; replace the file input element with the jquery-file-upload element.

--- app/components/wistia-uploader/wistia-uploader.component.js
+++ app/components/wistia-uploader/wistia-uploader.component.js
@@ -4,20 +4,25 @@ function WistiaUploaderController(wistiaService) {
     ctrl.err = null;
     ctrl.videoHashedId = null;
 
-    ctrl.onFileAdded = function (files) {
-        var file = files[0];
-        wistiaService.uploadFile(file, function (e) {
-            ctrl.progress = Math.floor(e.loaded / e.total * 100);
-        }).then(function (res) {
-            // {data: {hashed_id: "2uxmpksi7r"}}
-            ctrl.videoHashedId = res.data.hashed_id;
-            ctrl.progress = NaN;
-            ctrl.err = null;
-        }, function (err) {
-            // {data: {error: "This account has exceeded its video limit..."}}
-            ctrl.err = 'Wistia: ' + err.error;
-            ctrl.progress = NaN;
-        });
+    ctrl.$onInit = function () {
+        var input = angular.element('.fileinput-button > input')
+        input.fileupload({
+            add: function (e, data) {
+                var file = data.files[0];
+                wistiaService.uploadFile(file, function (e) {
+                    ctrl.progress = Math.floor(e.loaded / e.total * 100);
+                }).then(function (res) {
+                    // {data: {hashed_id: "2uxmpksi7r"}}
+                    ctrl.videoHashedId = res.data.hashed_id;
+                    ctrl.progress = NaN;
+                    ctrl.err = null;
+                }, function (err) {
+                    // {data: {error: "This account has exceeded its video limit..."}}
+                    ctrl.err = 'Wistia: ' + err.data.error;
+                    ctrl.progress = NaN;
+                });
+            }
+        })
     }
 }
 
