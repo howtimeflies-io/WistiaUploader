99c8c1ac9a6329d9f9a1bb928a2f3fab6ac748a7
Huang Jian <huang.jian@howtimeflies.io>
2018-07-01 11:23:02 +0800

    Delete the unused files; add the Wistia Uploader component skeleton.

---
+++ app/components/wistia-uploader/wistia-uploader.component.js
@@ -0,0 +1,9 @@
+function WistiaUploaderController() {
+    var ctrl = this;
+}
+
+
+angular.module('wistiaUploaderModule').component('wistiaUploader', {
+    templateUrl: 'components/wistia-uploader/wistia-uploader.component.html',
+    controller: WistiaUploaderController
+})
